2f67119ce375d8b4a20d0ed41b9b051e82d837a8
aaakashppandey <aaakashppandey@gmail.com>
2023-07-10 18:38:08 +0530

    Basics of react

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Welcome to React Learnings from basic 
+# "npm" does not stands for node package manager. it just manages the package
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("h1", {
+    id: "parent"
+}, React.createElement("h3", {
+    id: "div1"
+}, "I am inside Div 1"), React.createElement("h3", {
+    id: "div2"
+}, "I am inside Div 2"), React.createElement("h3", {
+    id: "div3"
+}, "I am inside Div 3"), "Hello World from React App JS ");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
